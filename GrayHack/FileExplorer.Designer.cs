8aec9b400eba6418bb9f95c3156a428a3be74468
n3z3r <kirill.mel07@gmail.com>
2022-02-09 15:58:32 +0600

    add file explorer form and fix bugs

---
+++ GrayHack/FileExplorer.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace GrayHack
+{
+    partial class FileExplorer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.closeButton = new System.Windows.Forms.ToolStripButton();
+            this.titleLabel = new System.Windows.Forms.ToolStripLabel();
+            this.FileManagerPanel = new System.Windows.Forms.Panel();
+            this.pathLabel = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(30)))), ((int)(((byte)(35)))));
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeButton,
+            this.titleLabel,
+            this.pathLabel});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MovingWindow);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.closeButton.BackgroundImage = global::GrayHack.Properties.Resources.Close;
+            this.closeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.closeButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.closeButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(23, 22);
+            this.closeButton.Text = "Close";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.titleLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(75, 22);
+            this.titleLabel.Text = "Message";
+            this.titleLabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MovingWindow);
+            // 
+            // FileManagerPanel
+            // 
+            this.FileManagerPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.FileManagerPanel.Location = new System.Drawing.Point(12, 36);
+            this.FileManagerPanel.Name = "FileManagerPanel";
+            this.FileManagerPanel.Size = new System.Drawing.Size(776, 402);
+            this.FileManagerPanel.TabIndex = 2;
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.pathLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(219)))), ((int)(((byte)(255)))));
+            this.pathLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pathLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(26, 22);
+            this.pathLabel.Text = "test";
+            // 
+            // FileExplorer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FileManagerPanel);
+            this.Controls.Add(this.toolStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FileExplorer";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "FileExplorer";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton closeButton;
+        private System.Windows.Forms.ToolStripLabel titleLabel;
+        private System.Windows.Forms.Panel FileManagerPanel;
+        private System.Windows.Forms.ToolStripLabel pathLabel;
+    }
+}
